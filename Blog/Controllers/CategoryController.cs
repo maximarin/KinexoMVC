2dcc6ea5aae62911bc3feb6d0ed77d357172aa57
maximarin <31014199+maximarin@users.noreply.github.com>
2018-09-19 23:38:41 -0300

    Comienzo del proyecto. Defino el modelo de Categorias, junto con la alta y modificación de las mismas.

---
+++ Blog/Controllers/CategoryController.cs
@@ -0,0 +1,70 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private static IList<Category> categories = new List<Category>();
+
+        public ActionResult Index()
+        {
+            var ejemplo1 = new Category { Name = "Futbol", Active = true, Description = "Primera División Argentina", Id = 1 };
+            var ejemplo2 = new Category { Name = "Tenis", Active = true, Description = "Tenis Argentino", Id = 2 };
+
+            categories.Add(ejemplo1);
+            categories.Add(ejemplo2);
+            return View(categories);
+        }  
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            model.Id = categories.Count + 1;
+            model.Active = true;
+
+            categories.Add(model);
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var category = categories.Where(x => x.Id == id).First();
+
+            return View(category); 
+        }
+
+        [HttpPost]
+        public ActionResult Edit (Category model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var category = categories.Where(x => x.Id == model.Id).First();
+
+            category.Name = model.Name;
+            category.Description = model.Description;
+            category.Active = model.Active;
+
+          
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Blog/Models/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Category
+    { 
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre de la Categoria")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Descripcion")]
+        [MaxLength(256)]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Activa")]
+        public bool Active { get; set; } 
+
+
+    }
+}
