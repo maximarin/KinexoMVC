a0ad24f0430308ad0d739cc11c9873893a80a318
maximarin <31014199+maximarin@users.noreply.github.com>
2018-10-05 00:40:12 -0300

    Modelado de UserModel

---
+++ Blog/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserModel model)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UserModel model)
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Blog/Models/UserModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Display(Name = "Apellido")]
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+
+        [Display(Name = "Usuario")]
+        [Required]
+        [MaxLength(50)]
+        public string Nick { get; set; }
+
+        [Display(Name = "Dirección de email")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Contraseña")]
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+    }
+}
